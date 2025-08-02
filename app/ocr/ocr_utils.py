0d072ded4c4c2db5a6719a20682172664dae1e25
Alonso AF <aaf2102@outlook.com>
2025-08-02 11:43:01 -0600

    changes

--- app/ocr/ocr_utils.py
+++ app/ocr/ocr_utils.py
@@ -1,2 +1,9 @@
+import cv2
+import pytesseract
 from utils.file_loader import load_file_image
 
+pytesseract.pytesseract.tesseract_cmd = r"D:\home\[00]_Projects\Extractor\tesseract.exe"
+
+def load_image(file_path):
+    image = load_file_image(file_path)
+    height, width = image.shape[:2]
